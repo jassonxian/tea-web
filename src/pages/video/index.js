9321e0e7463361fcf969a9421d8bc56b478c149d
mazhaoxian <173215552@qq.com>
2019-08-29 19:53:39 +0800

    添加首页宣传视频

--- src/pages/video/index.js
+++ src/pages/video/index.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Card, Upload, Button, Icon } from 'antd';
+import { Card, Upload, Button, Icon, Divider } from 'antd';
 import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 
 export default () => {
   const props = {
     action: '/api/upload/video',
     listType: 'picture',
-    previewFile(file) {
-      console.log('Your upload file:', file);
-      // Your process logic. Here we just mock to the same file
-      //   return fetch('/api/show/video', {
-      //     method: 'POST',
-      //     body: file,
-      //   })
-      //     .then(res => res.json())
-      //     .then(({ thumbnail }) => thumbnail);
-    },
+    // previewFile(file) {
+    // console.log('Your upload file:', file);
+    // Your process logic. Here we just mock to the same file
+    //   return fetch('/api/show/video', {
+    //     method: 'POST',
+    //     body: file,
+    //   })
+    //     .then(res => res.json())
+    //     .then(({ thumbnail }) => thumbnail);
+    // },
+  };
+  const uploadProps = {
+    action: '/api/upload/wx_index_video',
+    listType: 'picture',
   };
   return (
     <PageHeaderWrapper title="宣传视频">
       <Card bordered={false}>
         <div>
+          <Divider orientation="left">宣传视频</Divider>
           <Upload {...props}>
+            <Button>
+              <Icon type="upload" /> Upload
+            </Button>
+          </Upload>
+          <Divider orientation="left">首页视频</Divider>
+          <Upload {...uploadProps}>
             <Button>
               <Icon type="upload" /> Upload
             </Button>
